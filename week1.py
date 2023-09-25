354d08f78000a15cc0d23896eb716e7192af4ec6
Hoang@KISTpc <nguyenquochoang.ds@gmail.com>
2023-09-25 15:09:06 +0900

    colored rotating cube with controls

--- week1.py
+++ week1.py
@@ -1,92 +1,89 @@
 import pygame
 from pygame.locals import *
 from OpenGL.GL import *
-from OpenGL.GLUT import *
-import numpy as np
+from OpenGL.GLU import *
+
+vertices = (
+    (1, -1, -1),
+    (1, 1, -1),
+    (-1, 1, -1),
+    (-1, -1, -1),
+    (1, -1, 1),
+    (1, 1, 1),
+    (-1, 1, 1),
+    (-1, -1, 1),
+)
+edges = (
+    (0, 1),
+    (1, 2),
+    (2, 3),
+    (3, 0),
+    (4, 5),
+    (5, 6),
+    (6, 7),
+    (7, 4),
+    (0, 4),
+    (1, 5),
+    (2, 6),
+    (3, 7),
+)
+
+surfaces = (
+    (0, 1, 2, 3),
+    (0, 3, 7, 4),
+    (0, 1, 5, 4),
+    (6, 5, 4, 7),
+    (6, 2, 3, 7),
+    (6, 2, 1, 5),
+)
+
+# colors for vertices
+from itertools import product
+colors = tuple(product([1,0],[1,0],[1,0]))
+
+def draw_cube():
+    glBegin(GL_QUADS)
+    for surface in surfaces:
+        glColor3fv((0, 1, 0))
+        for vertex in surface:
+            glColor3fv(colors[vertex])
+            glVertex3fv(vertices[vertex])
+    glEnd()
+
+    glBegin(GL_LINES)
+    for edge in edges:
+        glColor3fv((0, 0, 0))
+        for vertex in edge:
+            glColor3fv(colors[-vertex])
+            glVertex3fv(vertices[vertex])
+    glEnd()
 
 
-# Initialize Pygame and OpenGL
-pygame.init()
-display = (800, 600)
-pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+def main():
+    pygame.init()
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
+    glTranslatef(0.0, 0.0, -5)
+    rotation_angle = 1
 
-# Create a simple vertex shader
-vertex_shader = """
-#version 330 core
-layout(location = 0) in vec4 position;
-void main()
-{
-    gl_Position = position;
-}
-"""
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_0:
+                    rotation_angle += 5
+                elif event.key == pygame.K_1:
+                    rotation_angle -= 5
 
-# Create a simple fragment shader
-fragment_shader = """
-#version 330 core
-out vec4 color;
-void main()
-{
-    color = vec4(1.0, 0.0, 0.0, 1.0); // Red color
-}
-"""
+        glRotatef(rotation_angle, 2, 1, 2)
 
-# Create and compile shaders
-vertex_shader_id = glCreateShader(GL_VERTEX_SHADER)
-glShaderSource(vertex_shader_id, vertex_shader)
-glCompileShader(vertex_shader_id)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        draw_cube()
+        pygame.display.flip()
+        pygame.time.wait(20)
 
-fragment_shader_id = glCreateShader(GL_FRAGMENT_SHADER)
-glShaderSource(fragment_shader_id, fragment_shader)
-glCompileShader(fragment_shader_id)
 
-# Create a shader program and link shaders
-shader_program_id = glCreateProgram()
-glAttachShader(shader_program_id, vertex_shader_id)
-glAttachShader(shader_program_id, fragment_shader_id)
-glLinkProgram(shader_program_id)
-
-# Create vertices for a simple triangle
-vertices = np.array([
-    0.0,  1.0, 0.0,
-   -1.0, -1.0, 0.0,
-    1.0, -1.0, 0.0
-], dtype=np.float32)
-
-# Create a Vertex Array Object (VAO)
-vao = glGenVertexArrays(1)
-glBindVertexArray(vao)
-
-# Create a Vertex Buffer Object (VBO)
-vbo = glGenBuffers(1)
-glBindBuffer(GL_ARRAY_BUFFER, vbo)
-glBufferData(GL_ARRAY_BUFFER, len(vertices) * 4, vertices, GL_STATIC_DRAW)
-
-# Specify the attribute pointer
-glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, None)
-glEnableVertexAttribArray(0)
-
-# Main loop
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            quit()
-
-    # Clear the screen
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-
-    # Use the shader program
-    glUseProgram(shader_program_id)
-
-    # Bind the VAO
-    glBindVertexArray(vao)
-
-    # Draw the triangle
-    glDrawArrays(GL_TRIANGLES, 0, 3)
-
-    # Unbind the VAO and shader program
-    glBindVertexArray(0)
-    glUseProgram(0)
-
-    pygame.display.flip()
-    pygame.time.wait(10)
+main()
