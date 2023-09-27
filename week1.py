3b33096d0aa27665ed77cf1975e1d2ad2e6c9e53
Hoang@KISTpc <nguyenquochoang.ds@gmail.com>
2023-09-27 13:42:16 +0900

    in progress

--- week1.py
+++ week1.py
@@ -4,6 +4,8 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from itertools import product
 import random
+import numpy as np
+
 
 vertices = (
     (1, -1, -1),
@@ -46,10 +48,15 @@ surfaces = (
 
 
 def random_colors(num_vertices=8, num_channels=3):
-    return [[random.random() for __ in range(num_channels)] for _ in range(num_vertices)]
+    return [
+        [random.random() for __ in range(num_channels)] for _ in range(num_vertices)
+    ]
+
+
 # random colors
 
 colors = random_colors()
+
 
 def draw_cube():
     glBegin(GL_QUADS)
@@ -69,13 +76,25 @@ def draw_cube():
     glEnd()
 
 
+def print_model_view_matrix():
+    model_view_mat = glGetDoublev(GL_MODELVIEW_MATRIX)
+    print(type(model_view_mat))
+    print(model_view_mat)
+
+
 def main():
     pygame.init()
     display = (800, 600)
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+
+    # print_model_view_matrix()
     gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
+    # print_model_view_matrix()
     glTranslatef(0.0, 0.0, -5)
+    # print_model_view_matrix()
     rotation_angle = 1
+    # exit()
+    rotating = False
 
     while True:
         for event in pygame.event.get():
@@ -90,8 +109,31 @@ def main():
                 elif event.key == pygame.K_c:
                     global colors
                     colors = random_colors()
+                elif event.key == pygame.K_p:
+                    # calculate
+                    model_view_mat = glGetDoublev(GL_MODELVIEW_MATRIX)  # 4x4
+                    vertx = np.array(vertices)  # 6x3
+                    vertx = np.pad(vertx, ((0, 0), (0, 1)), constant_values=1)  # 6x4
+                    transformed_vertx = vertx @ model_view_mat
+                    print(transformed_vertx[:, :3])
+                elif event.key == pygame.K_r:
+                    rotating = not rotating
 
-        glRotatef(rotation_angle, 2, 1, 2)
+                elif event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    quit()
+            elif event.type == pygame.MOUSEWHEEL:
+                # print(event)
+                # if event.button == pygame.BUTTON_WHEELDOWN:
+                if event.y > 0:
+                    glTranslatef(0, 0, 1)
+                if event.y < 0:
+                    glTranslatef(0, 0, -1)
+
+        if rotating:
+            glRotatef(rotation_angle, 0, 0, 0)
+            # glRotatef(rotation_angle, 2, 1, 2)
+        # print_model_view_matrix()
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         draw_cube()
